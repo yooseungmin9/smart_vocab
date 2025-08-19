ee691ffe544a49958f15d715bcebcbc798557ca4
“yooseungmin9” <2win0min@naver.com>
2025-08-19 12:10:29 +0900

    update

--- main_streamlit.py
+++ main_streamlit.py
@@ -16,7 +16,7 @@ if "word_correct_count" not in st.session_state:
 if "completed_words" not in st.session_state:
     st.session_state.completed_words = set()
 
-st.image("Logo.png", width=50)
+st.image("Logo.png", width=100)
 st.title("매일 매일 10개씩만 하자 - Yoo")
 st.write("Update: 2025.08.19")
 
