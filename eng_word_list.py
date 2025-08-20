f26b3b8131e376a0c8a232a7457020044a24ebe8
“yooseungmin9” <2win0min@naver.com>
2025-08-20 20:55:45 +0900

    update

--- eng_word_list.py
+++ eng_word_list.py
@@ -1,4 +1,4 @@
-eng_word_list_upper_intermediate = [
+eng_word_list = [
     {"word": "successful", "meanings": ["성공한", "잘된", "효과적인", "훌륭한"], "correct_meaning": "성공한", "accent": "석-세스풀"},
     {"word": "recommend", "meanings": ["추천하다", "권하다", "제안하다", "조언하다"], "correct_meaning": "추천하다", "accent": "리-커멘드"},
     {"word": "advantage", "meanings": ["장점", "이점", "유리함", "혜택"], "correct_meaning": "장점", "accent": "어드-밴티지"},
